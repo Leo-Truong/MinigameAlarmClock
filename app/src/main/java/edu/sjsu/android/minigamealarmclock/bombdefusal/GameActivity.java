ff863df3293b24c0cfb5cb130c58ac2f0de5ab75
Leo Truong <leo.truong@sjsu.edu>
2024-11-24 19:05:26 -0800

    Defuse the Bomb Nearly fully implemented the minigame

--- app/src/main/java/edu/sjsu/android/minigamealarmclock/bombdefusal/GameActivity.java
+++ app/src/main/java/edu/sjsu/android/minigamealarmclock/bombdefusal/GameActivity.java
@@ -1,6 +1,7 @@
 package edu.sjsu.android.minigamealarmclock.bombdefusal;
 
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.util.Log;
@@ -12,6 +13,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Objects;
 import java.util.Random;
 
 import edu.sjsu.android.minigamealarmclock.R;
@@ -24,137 +26,86 @@ public class GameActivity extends AppCompatActivity {
     private CountDownTimer countDownTimer;
     private final long timerDuration = 30000; // 30 seconds
     private final ArrayList<String> wires = new ArrayList<>(Arrays.asList("G", "R", "O1", "Y1", "B", "P", "O2", "Y2"));
-    private ArrayList<String> wiresToCut = new ArrayList<String>();
     private ArrayList<String> tempwires = new ArrayList<String>();
+    private ArrayList<String> wiresToCut = new ArrayList<String>();
     Random random;
     private int success = 0; // # of successful defusals
     private ActivityGameBinding binding;
+    private boolean wrongWire = false;
+    private Drawable originalBackground;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        GameView gameView = new GameView(this);
+//        setContentView(gameView);
 
         binding = ActivityGameBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        originalBackground = binding.BButton.getBackground();
 
         timerTextView = findViewById(R.id.timerTextView);
         wiresToCutTextView = findViewById(R.id.wiresToCutTextView);
 
-        // Disable buttons initially
-        binding.GButton.setEnabled(false);
-        binding.RButton.setEnabled(false);
-        binding.O1Button.setEnabled(false);
-        binding.Y1Button.setEnabled(false);
-        binding.BButton.setEnabled(false);
-        binding.PButton.setEnabled(false);
-        binding.O2Button.setEnabled(false);
-        binding.Y2Button.setEnabled(false);
-
         startGame();
-        binding.GButton.setOnClickListener(new View.OnClickListener() {
+    }
+
+    /**
+     * Reusable method to handle button click
+     * @param b Button that corresponds to its respective wire
+     * @param wireColor Color of wire the corresponds to each button
+     */
+    private void onButtonClicked(Button b, String wireColor) {
+        b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (wiresToCut.contains("G")){
-                    wiresToCut.remove("G");
-                    binding.GButton.setBackgroundColor(Color.GREEN);
-                    Log.d("GameActivty", "Removed G");
+                if (wiresToCut.contains(wireColor)) {
+                    wiresToCut.remove(wireColor);
+                    b.setBackgroundColor(Color.GREEN);
+                    b.setEnabled(false);
+                    Log.d("GameActivty", "Removed: " + wireColor);
                     Log.d("GameActivity", "wiresToCut: " + wiresToCut.toString());
+                }else{
+                    b.setBackgroundColor(Color.RED);
+                    disableAllButtons();
+                    wrongWire = true;
                 }
-
+                if (Objects.equals(wireColor, "G")){
+                    binding.wire1.setVisibility(View.INVISIBLE);
+                    binding.wirecut1.setVisibility(View.VISIBLE);
+                }
+                if (Objects.equals(wireColor, "R")){
+                    binding.wire2.setVisibility(View.INVISIBLE);
+                    binding.wirecut2.setVisibility(View.VISIBLE);
+                }
+                if (Objects.equals(wireColor, "O1")){
+                    binding.wire3.setVisibility(View.INVISIBLE);
+                    binding.wirecut3.setVisibility(View.VISIBLE);
+                }
+                if (Objects.equals(wireColor, "Y1")){
+                    binding.wire4.setVisibility(View.INVISIBLE);
+                    binding.wirecut4.setVisibility(View.VISIBLE);
+                }
             }
         });
+    }
 
-        binding.RButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (wiresToCut.contains("R")){
-                    wiresToCut.remove("R");
-                    binding.RButton.setBackgroundColor(Color.GREEN);
-                    Log.d("GameActivty", "Removed R");
-                    Log.d("GameActivity", "wiresToCut: " + wiresToCut.toString());
-                }
+//    private void checkWire(String wireColor, ImageView wire, ImageView wirecut){
+//        if (Objects.equals)
+//    }
 
-            }
-        });
-
-        binding.O1Button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (wiresToCut.contains("O1")){
-                    wiresToCut.remove("O1");
-                    binding.O1Button.setBackgroundColor(Color.GREEN);
-                    Log.d("GameActivty", "Removed O1");
-                    Log.d("GameActivity", "wiresToCut: " + wiresToCut.toString());
-                }
-
-            }
-        });
-
-        binding.Y1Button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (wiresToCut.contains("Y1")){
-                    wiresToCut.remove("Y1");
-                    binding.Y1Button.setBackgroundColor(Color.GREEN);
-                    Log.d("GameActivty", "Removed Y1");
-                    Log.d("GameActivity", "wiresToCut: " + wiresToCut.toString());
-                }
-
-            }
-        });
-
-        binding.BButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (wiresToCut.contains("B")){
-                    wiresToCut.remove("B");
-                    binding.BButton.setBackgroundColor(Color.GREEN);
-                    Log.d("GameActivty", "Removed B");
-                    Log.d("GameActivity", "wiresToCut: " + wiresToCut.toString());
-                }
-
-            }
-        });
-
-        binding.PButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (wiresToCut.contains("P")){
-                    wiresToCut.remove("P");
-                    binding.PButton.setBackgroundColor(Color.GREEN);
-                    Log.d("GameActivty", "Removed P");
-                    Log.d("GameActivity", "wiresToCut: " + wiresToCut.toString());
-                }
-
-            }
-        });
-
-        binding.O2Button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (wiresToCut.contains("O2")){
-                    wiresToCut.remove("O2");
-                    binding.O2Button.setBackgroundColor(Color.GREEN);
-                    Log.d("GameActivty", "Removed O2");
-                    Log.d("GameActivity", "wiresToCut: " + wiresToCut.toString());
-                }
-
-            }
-        });
-
-        binding.Y2Button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (wiresToCut.contains("Y2")){
-                    wiresToCut.remove("Y2");
-                    binding.Y2Button.setBackgroundColor(Color.GREEN);
-                    Log.d("GameActivty", "Removed Y2");
-                    Log.d("GameActivity", "wiresToCut: " + wiresToCut.toString());
-                }
-
-            }
-        });
-
+    /**
+     * Sets up all the buttons for the game
+     */
+    private void handleButtonLogic(){
+        onButtonClicked(binding.GButton, "G");
+        onButtonClicked(binding.RButton, "R");
+        onButtonClicked(binding.O1Button, "O1");
+        onButtonClicked(binding.Y1Button, "Y1");
+        onButtonClicked(binding.BButton, "B");
+        onButtonClicked(binding.PButton, "P");
+        onButtonClicked(binding.O2Button, "O2");
+        onButtonClicked(binding.Y2Button, "Y2");
 
     }
 
@@ -162,53 +113,77 @@ public class GameActivity extends AppCompatActivity {
      * Method to choose wires to cut
      */
     private void startGame() {
+        handleButtonLogic();
+        disableAllButtons();
         random = new Random();
-        tempwires = wires;
+        tempwires = new ArrayList<>(wires); // Shallow copy of wires
+
+        // Prepare visibility of wiresToCut and timer
+        if (wiresToCutTextView.getVisibility() == View.INVISIBLE)
+            wiresToCutTextView.setVisibility(View.VISIBLE);
+        if (timerTextView.getVisibility() == View.VISIBLE)
+            timerTextView.setVisibility(View.INVISIBLE);
 
         new CountDownTimer(5000, 1000) { // Total time of 5 seconds, tick every 1 second
 
             @Override
             public void onTick(long millisUntilFinished) {
-                int randInt = random.nextInt(tempwires.size());  // Generate random index
-                String wireToCut = tempwires.get(randInt);  // Get the wire to cut
-                wiresToCutTextView.setText(wireToCut);  // Display the wire
-                wiresToCut.add(wireToCut);  // Add to the wiresToCut list
-                tempwires.remove(randInt);  // Remove the wire from the list
+                if (!tempwires.isEmpty()) {
+                    int randInt = random.nextInt(tempwires.size());
+                    String wireToCut = tempwires.get(randInt);  // Get the wire to cut
+                    wiresToCutTextView.setText(wireToCut);  // Display the wire
+                    wiresToCut.add(wireToCut);  // Add to the wiresToCut list
+                    tempwires.remove(randInt);  // Remove the wire from the list
+                } else {
+                    Log.e("RandomError", "Bound must be positive!");
+                }
             }
 
             @Override
             public void onFinish() {
-                wiresToCutTextView.setVisibility(View.INVISIBLE);
-                // Enables all buttons
-                binding.GButton.setEnabled(true);
-                binding.RButton.setEnabled(true);
-                binding.O1Button.setEnabled(true);
-                binding.Y1Button.setEnabled(true);
-                binding.BButton.setEnabled(true);
-                binding.PButton.setEnabled(true);
-                binding.O2Button.setEnabled(true);
-                binding.Y2Button.setEnabled(true);
+                wiresToCutTextView.setVisibility(View.INVISIBLE); // Makes the the TextView invisible
+                enableAllButtons();  // Enables all buttons
 
                 Log.d("GameActivity", "wires: " + tempwires.toString());
                 Log.d("GameActivity", "wiresToCut: " + wiresToCut.toString());
-                startTimer();
+                startTimer(); // Begins timer for game
             }
         }.start();
     }
-
-
 
     /**
      * Method to keep track of the timer used in the bomb
      */
     private void startTimer() {
+        if (timerTextView.getVisibility() == View.INVISIBLE)
+            timerTextView.setVisibility(View.VISIBLE);
+
         countDownTimer = new CountDownTimer(timerDuration, 1000) {
             @Override
             public void onTick(long millisUntilFinished) {
                 timerTextView.setText("" + millisUntilFinished / 1000);
 
                 if(wiresToCut.isEmpty()){
+                    success += 1;
+                    if (success == 1 && binding.greendot1.getVisibility() == View.INVISIBLE){
+                        binding.greendot1.setVisibility(View.VISIBLE);
+                    }
+                    if (success == 2 && binding.greendot2.getVisibility() == View.INVISIBLE){
+                        binding.greendot2.setVisibility(View.VISIBLE);
+                    }
+                    if (success == 3 && binding.greendot3.getVisibility() == View.INVISIBLE){
+                        binding.greendot3.setVisibility(View.VISIBLE);
+                        finish();
+                    }
+                    Log.d("GameActivity", "success: " + success);
                     cancel();
+                    resetGame();
+                }
+                // Cancels the timer if wrong wire is cut
+                if (wrongWire){
+                    wrongWire = false;
+                    cancel();
+                    resetGame();
                 }
             }
 
@@ -222,6 +197,7 @@ public class GameActivity extends AppCompatActivity {
 
     private void onTimerFinished() {
         // Game over logic here
+        resetGame();
         Log.d("GameActivity", "Timer finished!");
     }
 
@@ -232,5 +208,83 @@ public class GameActivity extends AppCompatActivity {
         if (countDownTimer != null) {
             countDownTimer.cancel();
         }
+    }
+
+    /**
+     * Method to reset game logic
+     */
+    private void resetGame(){
+        wiresToCut.clear();
+        resetAllButtons();
+        resetAllWires();
+        startGame();
+    }
+
+    /**
+     * Method to disable all buttons that correlates with the wires
+     */
+    private void disableAllButtons(){
+        binding.GButton.setEnabled(false);
+        binding.RButton.setEnabled(false);
+        binding.O1Button.setEnabled(false);
+        binding.Y1Button.setEnabled(false);
+        binding.BButton.setEnabled(false);
+        binding.PButton.setEnabled(false);
+        binding.O2Button.setEnabled(false);
+        binding.Y2Button.setEnabled(false);
+    }
+
+    /**
+     * Method to enable all buttons that correlates with the wires
+     */
+    private void enableAllButtons(){
+        binding.GButton.setEnabled(true);
+        binding.RButton.setEnabled(true);
+        binding.O1Button.setEnabled(true);
+        binding.Y1Button.setEnabled(true);
+        binding.BButton.setEnabled(true);
+        binding.PButton.setEnabled(true);
+        binding.O2Button.setEnabled(true);
+        binding.Y2Button.setEnabled(true);
+    }
+
+    /**
+     * Method to reset all button backgrounds
+     */
+    private void resetAllButtons(){
+        binding.GButton.setBackground(originalBackground);
+        binding.RButton.setBackground(originalBackground);
+        binding.O1Button.setBackground(originalBackground);
+        binding.Y1Button.setBackground(originalBackground);
+        binding.BButton.setBackground(originalBackground);
+        binding.PButton.setBackground(originalBackground);
+        binding.O2Button.setBackground(originalBackground);
+        binding.Y2Button.setBackground(originalBackground);
+    }
+
+    /**
+     * Method to reset all wires
+     */
+    private void resetAllWires(){
+
+        // Set all wire pngs to VISIBLE
+        binding.wire1.setVisibility(View.VISIBLE);
+        binding.wire2.setVisibility(View.VISIBLE);
+        binding.wire3.setVisibility(View.VISIBLE);
+        binding.wire4.setVisibility(View.VISIBLE);
+        binding.wire5.setVisibility(View.VISIBLE);
+        binding.wire6.setVisibility(View.VISIBLE);
+        binding.wire7.setVisibility(View.VISIBLE);
+        binding.wire8.setVisibility(View.VISIBLE);
+
+        // Set all wirecut pngs to INVISIBLE
+        binding.wirecut1.setVisibility(View.INVISIBLE);
+        binding.wirecut2.setVisibility(View.INVISIBLE);
+        binding.wirecut3.setVisibility(View.INVISIBLE);
+        binding.wirecut4.setVisibility(View.INVISIBLE);
+        binding.wirecut5.setVisibility(View.INVISIBLE);
+        binding.wirecut6.setVisibility(View.INVISIBLE);
+        binding.wirecut7.setVisibility(View.INVISIBLE);
+        binding.wirecut8.setVisibility(View.INVISIBLE);
     }
 }
