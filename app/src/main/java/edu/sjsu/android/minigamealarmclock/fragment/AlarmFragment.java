2644220deb347830cfa4d2326f545d309ad1b928
Leo Truong <leo.truong@sjsu.edu>
2024-11-28 22:49:12 -0800

    Refactored Code and Added Comments

--- app/src/main/java/edu/sjsu/android/minigamealarmclock/fragment/AlarmFragment.java
+++ app/src/main/java/edu/sjsu/android/minigamealarmclock/fragment/AlarmFragment.java
@@ -2,9 +2,9 @@ package edu.sjsu.android.minigamealarmclock.fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.lifecycle.Observer;
 import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -14,8 +14,6 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
-import java.util.List;
 
 import edu.sjsu.android.minigamealarmclock.model.Alarm;
 import edu.sjsu.android.minigamealarmclock.adapter.MyAlarmRecyclerViewAdapter;
@@ -46,18 +44,15 @@ public class AlarmFragment extends Fragment implements OnToggleAlarmListener {
         super.onCreate(savedInstanceState);
         alarmRecyclerViewAdapter = new MyAlarmRecyclerViewAdapter(this);
         alarmsListViewModel = new ViewModelProvider(this).get(AlarmsListViewModel.class);
-        alarmsListViewModel.getAlarmsLiveData().observe(this, new Observer<List<Alarm>>() {
-            @Override
-            public void onChanged(List<Alarm> alarms) {
-                if (alarms != null) {
-                    alarmRecyclerViewAdapter.setAlarms(alarms);
-                }
+        alarmsListViewModel.getAlarmsLiveData().observe(this, alarms -> {
+            if (alarms != null) {
+                alarmRecyclerViewAdapter.setAlarms(alarms);
             }
         });
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         fragmentAlarmsListBinding = FragmentAlarmListBinding.inflate(inflater,container,false);
@@ -68,38 +63,51 @@ public class AlarmFragment extends Fragment implements OnToggleAlarmListener {
         alarmsRecyclerView.setAdapter(alarmRecyclerViewAdapter);
 
         addAlarm = fragmentAlarmsListBinding.fragmentListalarmsAddAlarm;
-        addAlarm.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Navigation.findNavController(v).navigate(R.id.action_alarmsListFragment_to_createAlarmFragment);
-            }
-        });
+        addAlarm.setOnClickListener(v -> Navigation.findNavController(v).navigate(R.id.action_alarmsListFragment_to_createAlarmFragment));
 
         return view;
     }
 
+    /**
+     * Method used when the switch is toggled. When toggled
+     * the user can cancel the alarm or schedule alarm. This
+     * will be reflected in the database
+     * @param alarm current alarm
+     */
     @Override
     public void onToggle(Alarm alarm) {
         if (alarm.isStarted()) {
-            alarm.cancelAlarm(getContext());
+            alarm.cancelAlarm(requireContext());
             alarmsListViewModel.update(alarm);
         } else {
-            alarm.schedule(getContext());
+            alarm.schedule(requireContext());
             alarmsListViewModel.update(alarm);
         }
     }
 
+    /**
+     * When the alarm is deleted the method cancels the alarm
+     * and removes the deleted alarm from the database.
+     * @param alarm current alarm
+     */
     @Override
     public void onDelete(Alarm alarm) {
         if (alarm.isStarted())
-            alarm.cancelAlarm(getContext());
+            alarm.cancelAlarm(requireContext());
         alarmsListViewModel.delete(alarm.getAlarmId());
     }
 
+    /**
+     * Method used when an alarm on the alarm list is clicked.
+     * The method cancels the current alarm and navigates the
+     * user to the createAlarmFragment.
+     * @param alarm current alarm
+     * @param view current view
+     */
     @Override
     public void onItemClick(Alarm alarm,View view) {
         if (alarm.isStarted())
-            alarm.cancelAlarm(getContext());
+            alarm.cancelAlarm(requireContext());
         Bundle args = new Bundle();
         args.putParcelable(getString(R.string.arg_alarm_obj),alarm);
         Navigation.findNavController(view).navigate(R.id.action_alarmsListFragment_to_createAlarmFragment,args);
