0f9f2ab5a75317321951a195c7e04d612fa336a7
Leo Truong <leo.truong@sjsu.edu>
2024-11-28 23:15:06 -0800

    Refactored Code and Added Comments

--- app/src/main/java/edu/sjsu/android/minigamealarmclock/broadcrastreceiver/AlarmBroadcastReceiver.java
+++ app/src/main/java/edu/sjsu/android/minigamealarmclock/broadcrastreceiver/AlarmBroadcastReceiver.java
@@ -19,16 +19,18 @@ public class AlarmBroadcastReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        // Used when device is rebooted and reschedules alarms that might've been lost
         if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
             String toastText = "Alarm Reboot";
             Toast.makeText(context, toastText, Toast.LENGTH_SHORT).show();
             startRescheduleAlarmsService(context);
         }
         else {
-            Bundle bundle=intent.getBundleExtra(context.getString(R.string.bundle_alarm_obj));
-            if (bundle!=null)
-                alarm = (Alarm) bundle.getParcelable(context.getString(R.string.arg_alarm_obj));
+            Bundle bundle = intent.getBundleExtra(context.getString(R.string.bundle_alarm_obj));
+            if (bundle != null)
+                alarm = bundle.getParcelable(context.getString(R.string.arg_alarm_obj));
 
+            // Show the alarm has been received and start alarm service
             String toastText = "Alarm Received";
             Toast.makeText(context, toastText, Toast.LENGTH_SHORT).show();
             if(alarm!=null) {
@@ -43,6 +45,11 @@ public class AlarmBroadcastReceiver extends BroadcastReceiver {
         }
     }
 
+    /**
+     * Method to determine if the alarm is set for the current day
+     * @param alarm1 alarm that is being checked
+     * @return return true if the alarm is for today otherwise false
+     */
     private boolean isAlarmToday(Alarm alarm1) {
         Calendar calendar = Calendar.getInstance();
         calendar.setTimeInMillis(System.currentTimeMillis());
@@ -67,6 +74,11 @@ public class AlarmBroadcastReceiver extends BroadcastReceiver {
         return false;
     }
 
+    /**
+     * Method to start the alarm service
+     * @param context current context
+     * @param alarm1 current alarm
+     */
     private void startAlarmService(Context context, Alarm alarm1) {
         Intent intentService = new Intent(context, AlarmService.class);
         Bundle bundle = new Bundle();
@@ -80,6 +92,10 @@ public class AlarmBroadcastReceiver extends BroadcastReceiver {
         }
     }
 
+    /**
+     * Method to begin the reschedule alarm service
+     * @param context current context
+     */
     private void startRescheduleAlarmsService(Context context) {
         Intent intentService = new Intent(context, RescheduleAlarmsService.class);
 
